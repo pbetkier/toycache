67418b8e66b7d470438556fe8e52091155e632ed
Piotr Betkier <pbetkier@gmail.com>
2021-02-11 16:15:25 +0100

    adds maxSize feature

--- toycache.go
+++ toycache.go
@@ -1,23 +1,62 @@
 package toycache
 
+import (
+	"container/list"
+)
+
 type ToyCache interface {
 	Put(string, interface{})
 	Get(string) (interface{}, bool)
 }
 
+const unlimitedSize = -1
+
 type toyCache struct {
-	content map[string]interface{}
+	content      map[string]interface{}
+	oldestWrites *list.List
+	maxSize      int
 }
 
-func New() ToyCache {
-	return &toyCache{make(map[string]interface{})}
+type writeExpire struct {
+	key string
+}
+
+func MaxSize(maxSize int) func(*toyCache) {
+	return func(configured *toyCache) {
+		configured.maxSize = maxSize
+	}
+}
+
+func New(options ...func(subject *toyCache)) ToyCache {
+	created := &toyCache{
+		content:      make(map[string]interface{}),
+		oldestWrites: list.New(),
+		maxSize:      unlimitedSize,
+	}
+
+	for _, o := range options {
+		o(created)
+	}
+
+	return created
 }
 
 func (c *toyCache) Put(key string, value interface{}) {
+	if len(c.content) == c.maxSize {
+		c.removeOldest()
+	}
+
+	c.oldestWrites.PushBack(writeExpire{key})
 	c.content[key] = value
 }
 
 func (c *toyCache) Get(key string) (interface{}, bool) {
 	val, ok := c.content[key]
 	return val, ok
+}
+
+func (c *toyCache) removeOldest() {
+	oldest := c.oldestWrites.Front()
+	delete(c.content, oldest.Value.(writeExpire).key)
+	c.oldestWrites.Remove(oldest)
 }
--- toycache_test.go
+++ toycache_test.go
@@ -26,3 +26,23 @@ func TestSimplePutAndGet(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, 1, val)
 }
+
+func TestCappedByMaxSizeRemovingOldestFirst(t *testing.T) {
+	// given
+	cache := New(MaxSize(2))
+	cache.Put("first", 1)
+	cache.Put("second", 2)
+	cache.Put("third", 3)
+
+	// when
+	_, firstOk := cache.Get("first")
+	secondVal, secondOk := cache.Get("second")
+	thirdVal, thirdOk := cache.Get("third")
+
+	// then
+	assert.False(t, firstOk)
+	assert.True(t, secondOk)
+	assert.Equal(t, 2, secondVal)
+	assert.True(t, thirdOk)
+	assert.Equal(t, 3, thirdVal)
+}
